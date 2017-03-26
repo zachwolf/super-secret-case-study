136f3fe19aed741b0735f35f180bdc7345e9f83a
Zach Wolf <hello@zachwolf.com>
2017-03-25 21:04:11 -0500

    Working server call

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,13 +2,48 @@ import React, { PropTypes, Component } from 'react'
 import logo from './logo.svg'
 import './index.css'
 
+function fetchProduct(id, cb) {
+  return fetch(`api/product/${id}`, {
+    accept: 'application/json',
+  }).then(checkStatus)
+    .then(response => response.json())
+    .then(cb)
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  }
+  const error = new Error(`HTTP Error ${response.statusText}`)
+  error.status = response.statusText
+  error.response = response
+  console.log(error) // eslint-disable-line no-console
+  throw error
+}
+
 class App extends Component {
   // static propTypes = {}
   // static defaultProps = {}
-  // state = {}
+  state = {
+    isLoading: true
+  }
+
+  componentDidMount() {
+    fetchProduct('1840', product => { // todo: id could be passed from react router
+      this.setState({
+        isLoading: false,
+        product
+      })
+    })
+  } 
 
   render () {
-    return (
+    const {
+      isLoading,
+      product
+    } = this.state
+
+    return isLoading ? null : (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
