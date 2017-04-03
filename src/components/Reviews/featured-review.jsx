e349f90222d241311adbef3cea95139d05578082
Zach Wolf <hello@zachwolf.com>
2017-04-02 22:24:44 -0500

    comment cleanup

--- src/components/Reviews/featured-review.jsx
+++ src/components/Reviews/featured-review.jsx
@@ -3,39 +3,6 @@ import Radium from 'radium'
 import { get } from 'lodash'
 import StarRating from './star-rating.jsx'
 import { COLOR } from '../common/styles.js'
-/*
-<div>
-  <div>
-    <span>Pro</span>
-    <h4>Most helpful 4-5 star review</h4>
-  </div>
-  <div>
-    <StarRating rated={ 5 } static />
-    <h5>title</h5>
-    <p>
-      review snippet
-    </p>
-    <div>
-      <a href="#">Name</a>
-      <span>date</span>
-    </div>
-  </div>
-</div>
-
-<div itemprop="review" itemscope itemtype="http://schema.org/Review">
-    <span itemprop="name">Not a happy camper</span> -
-    by <span itemprop="author">Ellie</span>,
-    <meta itemprop="datePublished" content="2011-04-01">April 1, 2011
-    <div itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating">
-      <meta itemprop="worstRating" content = "1">
-      <span itemprop="ratingValue">1</span>/
-      <span itemprop="bestRating">5</span>stars
-    </div>
-    <span itemprop="description">The lamp burned out and now I have to replace
-    it. </span>
-  </div>
-
- */
 
 const MONTH_LIST = [
   'January',
